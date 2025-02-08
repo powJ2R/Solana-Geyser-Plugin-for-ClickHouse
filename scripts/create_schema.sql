a2dd8c7cf1ed1d2cac446ec33d8ef6faa70f9ce4
Parvat Raj <parvat.raj2@gmail.com>
2025-02-08 11:48:41 +0530

    use replacingMergeTree version mangment

--- scripts/create_schema.sql
+++ scripts/create_schema.sql
@@ -1,25 +1,31 @@
-CREATE DATABASE IF NOT EXISTS solana;
-
-CREATE TABLE IF NOT EXISTS solana.accounts (
+CREATE TABLE accounts
+(
+    slot UInt64,
     pubkey String,
-    lamports UInt64,
     owner String,
-    executable Bool,
+    lamports UInt64,
+    executable UInt8,
     rent_epoch UInt64,
     data String,
-    write_version UInt64,
     updated_at DateTime64(3),
-    slot UInt64
-) ENGINE = MergeTree()
-ORDER BY (slot, pubkey);
+    txn_signature Nullable(String),
+    write_version UInt64
+)
+ENGINE = ReplacingMergeTree(write_version)
+ORDER BY (pubkey, slot);
 
-CREATE TABLE IF NOT EXISTS solana.transactions (
+CREATE TABLE transactions
+(
     signature String,
     slot UInt64,
-    success Bool,
-    fee UInt64,
     block_time DateTime64(3),
-    program_id String,
-    instructions Array(String)
-) ENGINE = MergeTree()
-ORDER BY (slot, signature);
+    err Nullable(String),
+    fee UInt64,
+    index UInt32,
+    programs Array(String),
+    accounts Array(String),
+    log_messages Array(String),
+    data String
+)
+ENGINE = MergeTree
+ORDER BY (slot, block_time);
